fdd3071e2af5b545b80d89a417ec1ac6ad875ef7
shrimay18 <shrimaysomani18@gmail.com>
2024-07-03 11:00:52 +0530

    implemented put-post function

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const express = require('express');
 const app = express();
 
-let courses = {
-    1: { id: 1, name: 'java' },
-    2: { id: 2, name: 'javascript' },
-    3: { id: 3, name: 'python' }
-};
+app.use(express.json());
+let courses = [
+    { id: 1, name: 'java' },
+    { id: 2, name: 'javascript' },
+    { id: 3, name: 'python' }
+];
 
 app.get('/courses',(req,res)=>{
     res.json(courses);
 });
 
+app.post('/courses',(req,res)=>{
+    console.log(req.body);
+    let singleCourse = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(singleCourse);
+    res.send(courses);
+});
+
+app.put('/courses/:id',(req,res)=>{
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with given id is not found');
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/courses/:id',(req,res)=>{
+    const userId = parseInt(req.params.id);
+    const userIndex = courses.findIndex(user => user.id === userId);
+
+    if (userIndex !== -1) {
+        courses.splice(userIndex, 1);
+        res.status(200).send({ message: 'User deleted successfully' });
+    } else {
+        res.status(404).send({ message: 'User not found' });
+    }
+});
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
 });
+
